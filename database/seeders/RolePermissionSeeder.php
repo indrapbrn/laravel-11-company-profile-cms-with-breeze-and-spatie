8e4905f498cfa8d40f2af5ab972e4a94433a9c1c
Kadek Indra <kadekindra784@gmail.com>
2025-12-14 18:27:46 +0800

    Initial commit: Company Profile Laravel 11 with Breeze, Spatie and CMS

---
+++ database/seeders/RolePermissionSeeder.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
+
+
+class RolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $permissions = [
+            'manage statistics',
+            'manage products',
+            'manage principles',
+            'manage clients',
+            'manage testimonials',
+            'manage teams',
+            'manage abouts',
+            'manage appointments',
+            'manage principles',
+            'manage hero sections',
+        ];
+
+        foreach($permissions as $permission){
+        Permission::firstOrCreate(
+            [
+            'name' =>  $permission
+            ] 
+            ); 
+        }
+
+        $designManagerRole = Role::firstOrCreate ([
+            'name' => 'designer_manager'
+        ]);
+
+        $designManagerPermissions= [
+            'manage products',
+            'manage principles',
+            'manage testimonials'
+        ];
+
+        $designManagerRole->syncPermissions($designManagerPermissions);
+
+        $superAdminRole = Role::firstOrCreate ([
+            'name' => 'super_admin'
+        ]);
+
+        $user = User::create([
+            'name' => 'Indra',
+            'email' => 'superadmin.com',
+            'password' => bcrypt('123123')
+        ]);
+
+        $user->assignRole($superAdminRole);
+    }
+}
