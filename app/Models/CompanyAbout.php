bee6570e7957013604c79c8967a460086afd8ee0
Kadek Indra <kadekindra784@gmail.com>
2025-12-15 15:29:17 +0800

    Add field and ORM

--- app/Models/CompanyAbout.php
+++ app/Models/CompanyAbout.php
@@ -2,9 +2,21 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class CompanyAbout extends Model
 {
-    //
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'thumbnail',
+        'type'
+    ];
+
+    public function keypoints() {
+        return $this->hasMany(CompanyKeypoint::class);
+    }
 }
